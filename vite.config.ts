ff1665096c644380a2f3503d8905ae9c65aa9043
William Cairns <cairnswm@gmail.com>
2025-05-05 08:42:04 +0200

    Change config to allow to run form sub directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Ensures assets are loaded relative to the current directory
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
