e082a89f5cc7106851fb6fb3a39cdd61b578c947
William Cairns <cairnswm@gmail.com>
2025-05-05 16:22:23 +0200

    Change build process to support sub directory deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,15 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: 'assets',
+  },
+  publicDir: 'public', // Ensures the public folder is copied to the dist folder
 });
